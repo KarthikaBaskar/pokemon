56c2be33bc43e86d9768638b87b3f27dfcf2fa1c
Karthika Baskar <kbaskar@popety.com>
2017-01-20 16:20:26 +0800

    navbar added

---
+++ app/components/navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, Navigator, TouchableHighlight } from 'react-native';
+import NavigationBar from 'react-native-navbar';
+
+export default class navbar extends Component {
+  render() {
+    return (
+       <NavigationBar
+         title={{ title:'PokeMap'}}
+         leftButton= {{ title:'Back'}}
+         rightButton={{ title:'Cancel'}}/>
+    );
+  }
+}
